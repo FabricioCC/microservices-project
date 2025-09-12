9c122b60264e84ea235cd534d9d7210e431a247c
Fabricio <fabriciocls2001@gmail.com>
2025-09-12 19:34:12 -0300

    Product Service created

---
+++ product-service/src/main/java/com/fabriciodev/microservices/product_service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.fabriciodev.microservices.product_service.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.fabriciodev.microservices.product_service.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    
+}
